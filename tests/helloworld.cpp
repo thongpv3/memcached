e3489317f7441a8c591c5d9820f4ef384dd49aae
Pham Van Thong <thongpv87@gmail.com>
2017-03-16 23:17:06 +0700

    init

--- tests/helloworld.cpp
+++ tests/helloworld.cpp
@@ -2,3 +2,14 @@
 // Created by thongpv87 on 16/03/2017.
 //
 
+#include <core/app-template.hh>
+#include <core/reactor.hh>
+#include <iostream>
+
+int main(int argc, char** argv) {
+    app_template app;
+    app.run(argc, argv, [] {
+        std::cout << "Hello world\n";
+        return make_ready_future<>();
+    });
+}
